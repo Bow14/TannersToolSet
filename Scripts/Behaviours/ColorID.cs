af445ae5d6105e8ab6c3019c8bd9a08c05bc38a2
TKBo <54337048+Bow14@users.noreply.github.com>
2022-10-08 19:28:05 -0600

    Add files via upload

---
+++ Scripts/Behaviours/ColorID.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class  ColorID : ID
+{
+    //    //AnthonyRomrell Module  Matching game 1 code
+
+    public Color value = Color.red;
+}
