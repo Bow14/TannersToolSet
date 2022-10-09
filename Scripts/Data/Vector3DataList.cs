f817840800d1a8b2cea402dfb78f74e4772d9da0
TKBo <54337048+Bow14@users.noreply.github.com>
2022-10-08 19:27:49 -0600

    Add files via upload

--- Scripts/Data/Vector3DataList.cs
+++ Scripts/Data/Vector3DataList.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[CreateAssetMenu]
-public class Vector3Data : ScriptableObject
-{
 
-    public Vector3 value;
+[CreateAssetMenu]
+public class Vector3DataList : ScriptableObject
+{
+  public List<Vector3Data> vector3List;
 }
